96fcfa0cc6d2accbb70593fc05bf64d5c7bc615f
hoangnn198 <66403260+hoangnn198@users.noreply.github.com>
2020-11-05 19:02:04 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -2,43 +2,73 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom";
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-
+import Slider from "./slider/Slider";
+import backgr1 from './image/backgr1.jpg'
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        {/* header nhen */}
-        <header className="Header">
-          <h1>Đây là Header</h1>
-        </header>
-
-        {/* thanh điều hướng */}
-        <nav className="Nav">
-          <a href="#">Trang chủ</a>
-          <a href="#">Giới thiệu</a>
-          <a href="#">Sản phẩm</a>
-          <a href="#">Liên hệ</a>
-        </nav>
-
-        {/* Slide ảnh */}
-        <div className="Slide">
-          <h1>Đây là slide ảnh</h1>
+      <div class="card">
+        <div class="card-header">
         </div>
-
-
-        <div className="Content">
-
+        <div class="topnav">
+          <a href="#">Làng Công nghệ</a>
+          <a href="#">Công nghệ</a>
+          <a href="#">Khoa học</a>
+          <a href="#">Công nghệ</a>
+          <a href="#">Khoa học</a>
         </div>
+        <div id="carouselId" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li
+                data-target="#carouselId"
+                data-slide-to="0"
+                class="active"
+              ></li>
+              <li data-target="#carouselId" data-slide-to="1"></li>
+              <li data-target="#carouselId" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img src={backgr1} alt="First slide" width = "100%" height = "300px"/>
+              </div>
 
-        <footer className="Footer">
-          <h1>Đây là footer</h1>
-        </footer>
+              <div class="carousel-item">
+              <img src={backgr1} alt="First slide" width = "100%" height = "300px"/>
+              </div>
 
-
+              <div class="carousel-item">
+              <img src={backgr1} alt="First slide" width = "100%" height = "300px"/>
+              </div>
+            </div>
+            <a
+              class="carousel-control-prev"
+              href="#carouselId"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a
+              class="carousel-control-next"
+              href="#carouselId"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        <div class="card-body">
+          <h4 class="card-title"> Title </h4> <p class="card-text"> Text </p>
+        </div>
+        <div class="card-footer text-muted"> Footer </div>
       </div>
     );
   }
