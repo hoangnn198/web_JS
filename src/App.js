c980ed5975495ae27c72299014b18288fbe4b004
hoangnn198 <66403260+hoangnn198@users.noreply.github.com>
2020-11-13 08:28:44 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -14,267 +14,43 @@ class App extends React.Component {
   render() {
     return (
       <div class="card">
-        <div class="header">
-          {/* nav  */}
-          <div class="nav">
-            <div
-              style={{ height: "100%", width: "25%" }}
-            >
-              <img
-                class="card-img-top"
-                src={logo}
-                alt=""
-                height="100%"
-                width="100%"
-              />
+        <div class="card-header">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <h4 style={{ color: 'red' }}>Cửa Gỗ Việt</h4>
+            <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+              aria-expanded="false" aria-label="Toggle navigation">
+              <img src={ic_menu} />
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                  <div class="dropdown-menu" aria-labelledby="dropdownId">
+                    <a class="dropdown-item" href="#">Action 1</a>
+                    <a class="dropdown-item" href="#">Action 2</a>
+                  </div>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" />
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
             </div>
-            <div
-              style={{ height: "50%", width: "70%" }}
-              id="right-nav"
-            >
-              <nav
-                class="navbar navbar-expand-lg navbar-light bg-light"
-                id="menu"
-              >
-                {/* <a class="navbar-brand" href="#">
-                  Navbar
-                </a> */}
-                <button
-                  class="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarNavDropdown"
-                  aria-controls="navbarNavDropdown"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                  <ul class="navbar-nav">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">
-                        Home <span class="sr-only">(current)</span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">
-                        Features
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">
-                        Pricing
-                      </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a
-                        class="nav-link dropdown-toggle"
-                        href="#"
-                        id="navbarDropdownMenuLink"
-                        role="button"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                      >
-                        Dropdown link
-                      </a>
-                      <div
-                        class="dropdown-menu"
-                        aria-labelledby="navbarDropdownMenuLink"
-                      >
-                        <a class="dropdown-item" href="#">
-                          Action
-                        </a>
-                        <a class="dropdown-item" href="#">
-                          Another action
-                        </a>
-                        <a class="dropdown-item" href="#">
-                          Something else here
-                        </a>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-            </div>
-          </div>
-
-          {/* slide  */}
-          <div class="silde">
-            <div id="carouselId" class="carousel slide" data-ride="carousel" height="100%">
-              <ol class="carousel-indicators">
-                <li data-target="#carouselId" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselId" data-slide-to="1"></li>
-                <li data-target="#carouselId" data-slide-to="2"></li>
-              </ol>
-              <div class="carousel-inner" role="listbox" height="100%">
-                <div class="carousel-item active">
-                  <img src={backgr1} alt="First slide" width="100%" />
-                </div>
-
-                <div class="carousel-item">
-                  <img src={backgr1} alt="First slide" width="100%" />
-                </div>
-
-                <div class="carousel-item">
-                  <img src={backgr1} alt="First slide" width="100%" />
-                </div>
-              </div>
-              <a
-                class="carousel-control-prev"
-                href="#carouselId"
-                role="button"
-                data-slide="prev"
-              >
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a
-                class="carousel-control-next"
-                href="#carouselId"
-                role="button"
-                data-slide="next"
-              >
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
-            </div>
-
-          </div>
+          </nav>
         </div>
-
-        {/* body  */}
         <div class="card-body">
-          <h4 class="card-title">Sản phẩm</h4>
-          <div class="row">
-            {/* main-content */}
-            <div class="main-content">
-              <div class="row">
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img1} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img2} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img1} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img1} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img2} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img1} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img1} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img2} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm">
-                  <div class="card-item">
-                    <img class="card-img-top" src={img1} alt="" />
-                    <div class="card-body">
-                      <h4 class="card-title">5.000.000 ₫</h4>
-                      <p class="card-text">Bộ Bàn Ăn 4 Ghế Gỗ Sồi</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <nav aria-label="Page navigation" id="pagination">
-                <ul class="pagination">
-                  <li class="page-item disabled">
-                    <a class="page-link" href="#" aria-label="Previous">
-                      <span aria-hidden="true">&laquo;</span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                  </li>
-                  <li class="page-item active">
-                    <a class="page-link" href="#">
-                      1
-                    </a>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">
-                      2
-                    </a>
-                  </li>
-                  <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                      <span aria-hidden="true">&raquo;</span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-
-            {/* right-content */}
-            <div class="right-content"></div>
-          </div>
+          <h4 class="card-title">Title</h4>
+          <p class="card-text">Text</p>
         </div>
-
-        {/* footer */}
-        <div class="card-footer text-muted">Footer</div>
+        <div class="card-footer text-muted">
+          Footer
+        </div>
       </div>
     );
   }
